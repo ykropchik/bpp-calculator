948765b649f620b158f48a41239f00bfea744e20
ykropchik <ykropchik22@gmail.com>
2026-04-16 12:07:15 +0500

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<div className="logo-mark"><span>BPP</span></div>
+			<div className="header-text">
+				<h1>Bits Per Pixel</h1>
+			</div>
+			<div className="version-tag">v1.0.0</div>
+		</header>
+	);
+}
+
+export default Header;
